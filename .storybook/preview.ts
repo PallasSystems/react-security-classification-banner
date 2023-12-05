ddd64ca67338164df5b4910ce8c21d1595b54c18
Stephen Crocker <stephen.crocker@pallas.uk>
2023-12-05 01:24:41 +0100

    Added storybook config

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,7 @@
 import type { Preview } from '@storybook/react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
