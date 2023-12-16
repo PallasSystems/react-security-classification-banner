c03ea2ed0731d2586d22b8a523ead582d0f6d86f
Stephen Crocker <stephen.crocker@pallas.uk>
2023-12-17 00:10:29 +0100

    Update indexes to have the correct methods

--- src/classification/index.ts
+++ src/classification/index.ts
@@ -1,3 +1,3 @@
 export type { Classification } from './classification.types';
 
-export { GetBannerStyle, GetClassificationText } from './classification';
+export { GetBannerStyle, GetClassificationText, MergeClassifications } from './classification';
--- src/country/index.ts
+++ src/country/index.ts
@@ -1,2 +1,3 @@
-export { ConvertCountryCode, GetISO3166Alpha3, GetOriginatingNation, GetEyesOnly } from './country';
+export { GetNationalityCaveatText, GetOriginatingNationText } from './country.text';
+export { MergeNationalityCaveat, MergeHandlingNation } from './country.utils';
 export type { Organisation, ISO_3166_1_ALPHA3, ISO_3166_1_ALPHA2 } from './country.types';
