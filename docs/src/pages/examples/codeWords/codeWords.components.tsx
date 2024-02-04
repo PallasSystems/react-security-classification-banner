d0f0f20f89344f13bb9080d8563fdc19d0e94592
Stephen Crocker <stephen.crocker@pallas.uk>
2024-02-04 21:36:37 +0100

    Added a basic Codeword example to the docs@

---
+++ docs/src/pages/examples/codeWords/codeWords.components.tsx
@@ -0,0 +1,71 @@
+import React, { FC, useEffect } from 'react';
+import { Container, Row } from 'react-bootstrap';
+import Prism from 'prismjs';
+
+import { BannerNavBar, Footer } from '@pallassystems/website-core';
+import { SecurityBanner } from '@pallassystems/react-security-classification-banner';
+
+// Getting Started Page Properties
+import { ExamplesPageProperties } from '../examples.types';
+import { GetBannerExamples } from './codeWords.data';
+
+// For code highlighting
+import 'prismjs/themes/prism.min.css';
+
+const CodeWordsBannerExamplesPage: FC<ExamplesPageProperties> = ({ footerProps, navBarProps }) => {
+  return (
+    <>
+      <BannerNavBar
+        {...navBarProps}
+        prefixRow={() => {
+          return <SecurityBanner />;
+        }}
+      />
+      <main className={'h-100 flex-grow-1'} role={'main'}>
+        <Container fluid id={'simple.container'} className={'content'}>
+          <CodeWordsBannerExamplesComponent />
+        </Container>
+      </main>
+      <Footer {...footerProps} />
+    </>
+  );
+};
+
+const CodeWordsBannerExamplesComponent: FC = () => {
+  useEffect(() => {
+    Prism.highlightAll();
+  });
+
+  return (
+    <Container>
+      <Row>
+        <h2>Create Security Banners with Codewords</h2>
+      </Row>
+      <Row>
+        <p>
+          The React Classification Banner aims to provide a UK Security Classification banner which will default to the
+          most basic security classification banner. In this example we are showcasing the default behavour it the
+          table.
+        </p>
+      </Row>
+      <Row>
+        <h3>Demonstration</h3>
+      </Row>
+      <Row>
+        <SecurityBanner records={{ classification: 'OFFICIAL', codeWords: ['CODE', 'EXAMPLE', 'WORDS'] }} />
+        <SecurityBanner records={{ classification: 'SECRET', codeWords: ['CODE', 'EXAMPLE', 'WORDS'] }} />
+        <SecurityBanner records={{ classification: 'TOP SECRET', codeWords: ['CODE', 'EXAMPLE', 'WORDS'] }} />
+      </Row>
+      <Row>
+        <h5>Source Code</h5>
+      </Row>
+      <Row>
+        <pre className={'language-javascript'}>
+          <code>{GetBannerExamples()}</code>
+        </pre>
+      </Row>
+    </Container>
+  );
+};
+
+export { CodeWordsBannerExamplesPage, CodeWordsBannerExamplesComponent };
--- docs/src/pages/examples/example.data.ts
+++ docs/src/pages/examples/example.data.ts
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import { ExamplesPage, ExamplesPageProperties } from './examples.types';
 import { SimpleBannerExamplesPage } from './simple';
+import { CodeWordsBannerExamplesPage } from './codeWords';
 
 export const ExamplePageData: ExamplesPage[] = [
   {
@@ -8,6 +9,13 @@ export const ExamplePageData: ExamplesPage[] = [
     text: 'Simple Examples',
     page: (data: ExamplesPageProperties): ReactNode => {
       return SimpleBannerExamplesPage(data);
+    },
+  },
+  {
+    link: '/example/codewords',
+    text: 'Code Word Examples',
+    page: (data: ExamplesPageProperties): ReactNode => {
+      return CodeWordsBannerExamplesPage(data);
     },
   },
 ];
