265c64dbed057c1a6dffaf933b99cca25e0e6596
Stephen Crocker <stephen.crocker@pallas.uk>
2024-08-31 21:23:55 +0100

    Updated to include the examples from the RBT project.

--- docs/src/App.data.ts
+++ docs/src/App.data.ts
@@ -1,9 +1,64 @@
 import { ReactNode } from 'react';
 
-import { ImageProperty } from '@pallassystems/website-core';
+import { ImageProperty, NavbarLinkProperty } from '@pallassystems/website-core';
 
 import { HomePageProperties } from './pages';
 import { getRBTIcon } from './App.icon';
+
+export const ExamplePageNavData: NavbarLinkProperty[] = [
+  {
+    path: '/examples/quick',
+    id: 'quick-example-page',
+    text: 'Quick Examples',
+    items: [
+      { path: '/examples/basic', id: 'react-bootstrap-table-table--basic-example', text: 'Basic Example', items: [] },
+      {
+        path: '/example/minimal',
+        id: 'react-bootstrap-table-table--minimal-example',
+        text: 'Minimal Example',
+        items: [],
+      },
+    ],
+  },
+  {
+    path: '/examples/fundamentals',
+    id: 'fundamentals-example-page',
+    text: 'Fundamentals',
+    items: [
+      {
+        path: '/examples/fundamentals/modifiedAccessorFN',
+        id: 'react-bootstrap-table-table--accessor-functions',
+        text: 'Making use of Accessor Functions',
+        items: [],
+      },
+      {
+        path: '/examples/fundamentals/modifiedCellFN',
+        id: 'react-bootstrap-table-table--modified-cell-functions',
+        text: 'Modifying Table Cells',
+        items: [],
+      },
+      {
+        path: '/examples/fundamentals/modifiedHeaderFN',
+        id: 'react-bootstrap-table-table--modified-header-functions',
+        text: 'Modifying Table Headers',
+        items: [],
+      },
+    ],
+  },
+  {
+    path: '/examples/styling',
+    id: 'style-example-page',
+    text: 'Styling',
+    items: [
+      {
+        path: '/examples/styling/varients',
+        id: 'react-bootstrap-table-table--table-varients',
+        text: 'Making use of Accessor Functions',
+        items: [],
+      },
+    ],
+  },
+];
 
 export const PageData: HomePageProperties = {
   navBarProps: {
@@ -20,9 +75,14 @@ export const PageData: HomePageProperties = {
       },
     },
     items: [
-      { path: '/gettingStarted', text: 'Getting Started' },
-      { path: '/api', text: 'API' },
-      { path: '/examples', text: 'Examples' },
+      { path: '/gettingStarted', id:'', text: 'Getting Started' },
+      { path: '/api', id:'', text: 'API' },
+      {
+        text: 'Examples',
+        path: '/examples',
+        id: '',
+        items: ExamplePageNavData,
+      }
     ],
     scm: {
       project: 'PallasSystems',
--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -4,17 +4,30 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import {
   ApiHeadPage,
   ApiTablePageData,
-  ExamplesHeadPage,
-  ExamplePageData,
   GettingStartedInstallPage,
   GettingStartedPage,
   GettingStartedUsagePage,
   HomePage,
 } from './pages';
-import { PageData } from './App.data';
+import { ExamplePageNavData, PageData } from './App.data';
 import { ApiTableTypesPageData } from './pages/api/api.data';
 
+import {
+  ExamplesHeadPage,
+  ExamplesSectionPage,
+  ExamplesStorybookPage,
+  NavbarLinkProperty,
+} from '@pallassystems/website-core';
+
+
 function App() {
+  let storybooks: NavbarLinkProperty[] = [];
+  ExamplePageNavData.forEach((value) => {
+    if (value.items && value.items.length > 0) {
+      storybooks = storybooks.concat(value.items);
+    }
+  });
+  
   return (
     <HashRouter>
       <Routes>
@@ -26,9 +39,12 @@ function App() {
         {ApiTableTypesPageData.map((value) => {
           return <Route key={value.text} path={value.link} element={value.page(PageData)} />;
         })}
-        <Route path={'/examples'} element={<ExamplesHeadPage {...PageData} />} />
-        {ExamplePageData.map((value) => {
-          return <Route key={value.text} path={value.link} element={value.page(PageData)} />;
+        <Route path={'/examples'} element={<ExamplesHeadPage {...PageData} exampleProps={ExamplePageNavData} />} />
+        {ExamplePageNavData.map((value: NavbarLinkProperty) => {
+          return <Route path={value.path} element={<ExamplesSectionPage exampleProps={value} {...PageData} />} />;
+        })}
+        {storybooks.map((value: NavbarLinkProperty) => {
+          return <Route path={value.path} element={<ExamplesStorybookPage exampleProps={value} {...PageData} />} />;
         })}
         <Route path={'/gettingstarted'} element={<GettingStartedPage {...PageData} />} />
         <Route path={'/gettingstarted/install'} element={<GettingStartedInstallPage {...PageData} />} />
