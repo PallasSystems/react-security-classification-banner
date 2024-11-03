39949c18f749d8f5b1701e14801a162a4bf50db3
Stephen Crocker <stephen.crocker@pallas.uk>
2024-11-03 20:23:27 +0000

    Updated the Page layout to use the templates

--- docs/src/App.data.ts
+++ docs/src/App.data.ts
@@ -1,9 +1,58 @@
 import { ReactNode } from 'react';
 
-import { ImageProperty } from '@pallassystems/website-core';
+import { ImageProperty, NavbarLinkProperty } from '@pallassystems/website-core';
 
 import { HomePageProperties } from './pages';
 import { getRBTIcon } from './App.icon';
+
+export const ExamplePageNavData: NavbarLinkProperty[] = [
+  {
+    path: '/examples/quick',
+    id: 'quick-example-page',
+    text: 'Quick Examples',
+    items: [
+      { path: '/examples/basic', id: 'react-uk-classification-banner-banner--basic-example', text: 'Basic Example', items: [] },
+      {
+        path: '/example/minimal',
+        id: 'react-uk-classification-banner-banner--minimal-example',
+        text: 'Minimal Example',
+        items: [],
+      },
+    ],
+  },
+  {
+    path: '/examples/fundamentals',
+    id: 'fundamentals-example-page',
+    text: 'Fundamentals',
+    items: [
+      {
+        path: '/examples/fundamentals/codewords',
+        id: 'react-uk-classification-banner-banner--codeword-examples',
+        text: 'Codeword Examples',
+        items: [],
+      },
+      {
+        path: '/examples/fundamentals/descriptors',
+        id: 'react-uk-classification-banner-banner--descriptor-examples',
+        text: 'Descriptor Examples',
+        items: [],
+      }
+    ],
+  },
+  {
+    path: '/examples/styling',
+    id: 'style-example-page',
+    text: 'Styling',
+    items: [
+      {
+        path: '/examples/styling/varients',
+        id: 'react-bootstrap-table-table--table-varients',
+        text: 'Making use of Accessor Functions',
+        items: [],
+      },
+    ],
+  },
+];
 
 export const PageData: HomePageProperties = {
   navBarProps: {
@@ -20,9 +69,14 @@ export const PageData: HomePageProperties = {
       },
     },
     items: [
-      { path: '/gettingStarted', text: 'Getting Started' },
-      { path: '/api', text: 'API' },
-      { path: '/examples', text: 'Examples' },
+      { path: '/gettingStarted', id:'', text: 'Getting Started' },
+      { path: '/api', id:'', text: 'API' },
+      {
+        text: 'Examples',
+        path: '/examples',
+        id: '',
+        items: ExamplePageNavData,
+      }
     ],
     scm: {
       project: 'PallasSystems',
