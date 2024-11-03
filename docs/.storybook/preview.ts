cd05369e8f33e0e26079435a4cfff869d6142b93
Stephen Crocker <stephen.crocker@pallas.uk>
2024-11-03 20:23:06 +0000

    Updated the storybook settings based on react-bootstrap-table

--- docs/.storybook/preview.ts
+++ docs/.storybook/preview.ts
@@ -1,9 +1,11 @@
 import type { Preview } from '@storybook/react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@pallassystems/website-core/website.css';
 
 const preview: Preview = {
   parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
